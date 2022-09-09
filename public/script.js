ad3854542f28f73dd9b3ad366baf875a389a7c15
Abdelrahman Hamdy <abdelrahmanhamdy49@gmail.com>
2022-09-09 22:48:52 +0200

    Connect to New User

--- public/script.js
+++ public/script.js
@@ -15,15 +15,34 @@ navigator.mediaDevices
   })
   .then((stream) => {
     addVideoStream(myVideo, stream);
+
+    myPeer.on("call", (call) => {
+      call.answer(stream);
+      const video = document.createElement("video");
+      call.on("stream", (userVideoStream) => {
+        addVideoStream(video, userVideoStream);
+      });
+    });
+
+    socket.on("user-connected", (userId) => {
+      connectToNewUser(userId, stream);
+    });
   });
 
 myPeer.on("open", (id) => {
   socket.emit("join-room", ROOM_ID, id);
 });
 
-socket.on("user-connected", (userId) => {
-  console.log("User connected: " + userId);
-});
+function connectToNewUser(userId, stream) {
+  const call = myPeer.call(userId, stream);
+  const video = document.createElement("video");
+  call.on("stream", (userVideoStream) => {
+    addVideoStream(video, userVideoStream);
+  });
+  call.on("close", () => {
+    video.remove();
+  });
+}
 
 function addVideoStream(video, stream) {
   video.srcObject = stream;
