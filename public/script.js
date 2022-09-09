bbc556492b9a95913f89c394b90034747d88da9e
Abdelrahman Hamdy <abdelrahmanhamdy49@gmail.com>
2022-09-09 22:34:10 +0200

    Adding video stream

--- public/script.js
+++ public/script.js
@@ -1,8 +1,21 @@
 const socket = io("/");
+const videoGrid = document.querySelector("#video-grid");
 const myPeer = new Peer(undefined, {
   host: "/",
   port: "3001",
 });
+
+const myVideo = document.createElement("video");
+myVideo.muted = true;
+
+navigator.mediaDevices
+  .getUserMedia({
+    video: true,
+    audio: true,
+  })
+  .then((stream) => {
+    addVideoStream(myVideo, stream);
+  });
 
 myPeer.on("open", (id) => {
   socket.emit("join-room", ROOM_ID, id);
@@ -11,3 +24,11 @@ myPeer.on("open", (id) => {
 socket.on("user-connected", (userId) => {
   console.log("User connected: " + userId);
 });
+
+function addVideoStream(video, stream) {
+  video.srcObject = stream;
+  video.addEventListener("loadedmetadata", () => {
+    video.play();
+  });
+  videoGrid.append(video);
+}
--- server.js
+++ server.js
@@ -1,3 +1,4 @@
+// Init
 const express = require("express");
 const app = express();
 const server = require("http").Server(app);
@@ -7,10 +8,9 @@ const { v4: uuidV4 } = require("uuid");
 app.set("view-engine", "ejs");
 app.use(express.static("public"));
 
-app,
-  get("/", (req, res) => {
-    res.redirect(`/${uuidV4()}`);
-  });
+app.get("/", (req, res) => {
+  res.redirect(`/${uuidV4()}`);
+});
 
 app.get("/:room", (req, res) => {
   res.render("room", { roomId: req.params.room });
