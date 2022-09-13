7ba875ff690c28c34a2dd7bca5a45de7533e2838
Abdelrahman Hamdy <abdelrahmanhamdy49@gmail.com>
2022-09-13 04:20:53 +0200

    Comments

--- public/script.js
+++ public/script.js
@@ -1,10 +1,22 @@
+// Get a reference to socket
 const socket = io("/");
+// Get the video grid element
 const videoGrid = document.getElementById("video-grid");
-const myPeer = new Peer();
+// PeerJS handles the ID connection
+// Creates connection between different users using webRTC and a server setup that creates these dynamic IDs
+const myPeer = new Peer(undefined, {
+  host: "/",
+  port: "3001",
+});
 
+// Create our own video
 const myVideo = document.createElement("video");
+// Mute ourselves
 myVideo.muted = true;
+// Object that contains all the call peers
 const peers = {};
+
+// Connect the video (returns a promise)
 navigator.mediaDevices
   .getUserMedia({
     video: true,
@@ -13,39 +25,49 @@ navigator.mediaDevices
   .then((stream) => {
     addVideoStream(myVideo, stream);
 
+    // Listen to the call event
     myPeer.on("call", (call) => {
+      // Answer the call and send them our stream
       call.answer(stream);
+      // Create a video element
       const video = document.createElement("video");
+      // Respond to any video streams
       call.on("stream", (userVideoStream) => {
         addVideoStream(video, userVideoStream);
       });
     });
-
+    // We need to be able to connect to other new users when they join
     socket.on("user-connected", (userId) => {
       connectToNewUser(userId, stream);
     });
   });
 
+// Listen to the disconnect event and close the call
 socket.on("user-disconnected", (userId) => {
   peers[userId]?.close();
 });
 
+// Call the join room event when the peer server is running
 myPeer.on("open", (id) => {
   socket.emit("join-room", ROOM_ID, id);
 });
 
 const connectToNewUser = (userId, stream) => {
+  // Call a user with the given ID and send our video stream
   const call = myPeer.call(userId, stream);
   const video = document.createElement("video");
+  // Listen to the stream event called when the user sends back their video stream and add it to our list of videos
   call.on("stream", (userVideoStream) => {
     addVideoStream(video, userVideoStream);
   });
+  // Remove video on the close event
   call.on("close", () => {
     video.remove();
   });
   peers[userId] = call;
 };
 
+// Take the video, play it and add it to the grid
 const addVideoStream = (video, stream) => {
   video.srcObject = stream;
   video.addEventListener("loadedmetadata", () => {
--- server.js
+++ server.js
@@ -1,29 +1,44 @@
-// Init
+// ***********Getting the server running***************
+
 const express = require("express");
 const app = express();
+// Create a server to be used for socket IO
 const server = require("http").Server(app);
+// Make socket IO interact with the server provided
 const io = require("socket.io")(server);
+// Generates a random room
 const { v4: uuidV4 } = require("uuid");
 
+// Setup the express server to render our views
 app.set("view-engine", "ejs");
+// Static Folder (public directory for all the JS and CSS)
 app.use(express.static("public"));
 
+// Home route
 app.get("/", (req, res) => {
   res.redirect(`/${uuidV4()}`);
 });
 
+// Dynamic route for the rooms
 app.get("/:room", (req, res) => {
   res.render("room.ejs", { roomId: req.params.room });
 });
 
+// This will run whenever someone connects to the web page
 io.on("connection", (socket) => {
+  // Set up the joining room event
   socket.on("join-room", (roomId, userId) => {
+    // Current socket joins the room provided
     socket.join(roomId);
+    // Sends a message to everyone else in the room that a new user connected
     socket.to(roomId).emit("user-connected", userId);
+    // Disconnect event
     socket.on("disconnect", () => {
+      // Sends a message to everyone else in the room that a new user disconnected
       socket.to(roomId).emit("user-disconnected", userId);
     });
   });
 });
 
+// Start the server on port 3000
 server.listen(3000);
